6d23a9b9fae87d7ab28bda0ca24e4b7e572ca7d3
Anna Veselova <anna.veselova.97@mail.ru>
2023-05-22 21:38:46 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,7 +7,7 @@ load_dotenv()
 telegram_token = os.getenv('TELEGRAM_TOKEN')
 chat_id = os.getenv('CHAT_ID')
 bot = telegram.Bot(token=telegram_token)
-bot.send_message(chat_id=chat_id, text="Это пробное сообщение")
-
+#bot.send_message(chat_id=chat_id, text="Это пробное сообщение")
+bot.send_document(chat_id=chat_id, document=open('images/nasa_apod5.jpg', 'rb'))
 
 
