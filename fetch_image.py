91f5f1cffa1109680fd0080128dc3ffbe5f56385
Anna Veselova <anna.veselova.97@mail.ru>
2023-05-25 22:19:47 +0300

    fix default argument

--- fetch_image.py
+++ fetch_image.py
@@ -2,7 +2,9 @@ import requests
 import os
 
 
-def fetch_image(image_name, url, payload={}, folder='images/'):
+def fetch_image(image_name, url, payload=None, folder='images/'):
+	if payload is None:
+		payload = {}
 	response = requests.get(url, params=payload)
 	response.raise_for_status()
 	image_path = os.path.join(folder, image_name)
