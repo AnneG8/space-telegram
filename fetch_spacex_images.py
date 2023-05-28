9a42faeac66964187d5ce9d641a1ab47fff5bcc1
Anna Veselova <anna.veselova.97@mail.ru>
2023-05-28 13:48:13 +0300

    fix fetch_spacex_launch

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -6,16 +6,19 @@ from fetch_image import fetch_image
 import pprint
 
 
-def fetch_spacex_launch(launch_id=0):
+def fetch_spacex_launch(launch_id):
 	base_url = 'https://api.spacexdata.com/v5/launches/'
 	if launch_id:
-		payload = {'id': args.id}
+		payload = {'id': launch_id}
 		response = requests.get(base_url, params=payload)
 	else:
 		url = urljoin(base_url, 'latest')
 		response = requests.get(url)
 	response.raise_for_status()
-	images = response.json()['links']['flickr']['original']
+	images = list()
+	for answ in response.json():
+		for url in answ['links']['flickr']['original']:
+			images.append(url)
 	for image_num, image_url in enumerate(images):
 		fetch_image(f'spacex{image_num}.jpg', image_url)
 
@@ -27,8 +30,8 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument(
 		'-id', 
-		type=int,
-		default=0, const=0, 
+		type=str,
+		default='', const='', 
         nargs='?'
     )
 	args = parser.parse_args()
